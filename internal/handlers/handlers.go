140723ca9d2c073103ea0a6f294ea1ab3e8f54aa
Aiden <aidenpham@ucsb.edu>
2025-08-07 13:53:50 -0700

    implemented create post and get all posts

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,8 +1,11 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
 
+	_ "github.com/phamaiden/blog-platform-api/internal/db"
+	"github.com/phamaiden/blog-platform-api/internal/models"
 	"github.com/phamaiden/blog-platform-api/internal/services"
 )
 
@@ -16,14 +19,50 @@ func NewBlogHandler(bs services.BlogService) *BlogHandler {
 	}
 }
 
-func (bh *BlogHandler) PostBlog(w http.ResponseWriter, r *http.Request) {
+func (bh *BlogHandler) GetAllBlogs(w http.ResponseWriter, r *http.Request) {
+	posts, err := bh.blogService.ReadAllPosts(r.Context())
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": err.Error(),
+		})
+		return
+	}
 
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(posts)
+}
+
+func (bh *BlogHandler) PostBlog(w http.ResponseWriter, r *http.Request) {
+	var request models.CreatePost
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	post, err := bh.blogService.CreatePost(r.Context(), &request)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(post)
 }
 
 func (bh *BlogHandler) PutBlog(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (bh *BlogHandler) GetBlog(w http.ResponseWriter, r *http.Request) {}
+func (bh *BlogHandler) GetBlogById(w http.ResponseWriter, r *http.Request) {
 
-func (bh *BlogHandler) DeleteBlog(w http.ResponseWriter, r *http.Request)
+}
+
+func (bh *BlogHandler) DeleteBlog(w http.ResponseWriter, r *http.Request) {}
--- internal/services/services.go
+++ internal/services/services.go
@@ -1,38 +1,60 @@
 package services
 
 import (
-	"github.com/jackc/pgx/v5"
+	"context"
+
+	"github.com/phamaiden/blog-platform-api/internal/db"
+	"github.com/phamaiden/blog-platform-api/internal/models"
 )
 
 type BlogService interface {
-	PostBlog()
-	PutBlog()
-	GetBlog()
-	DeleteBlog()
+	CreatePost(ctx context.Context, post *models.CreatePost) (*db.Post, error)
+	ReadAllPosts(ctx context.Context) (*[]db.Post, error)
+	ReadPostById(ctx context.Context) error
+	ReadPostByFilter(ctx context.Context) error
+	UpdatePost(ctx context.Context) error
+	DeleteBlog(ctx context.Context) error
 }
 
 type blogService struct {
-	dbConn *pgx.Conn
+	dbQueries *db.Queries
 }
 
-func NewBlogService(db *pgx.Conn) *blogService {
+func NewBlogService(q *db.Queries) *blogService {
 	return &blogService{
-		dbConn: db,
+		dbQueries: q,
 	}
 }
 
-func (bs *blogService) PostBlog() {
+func (bs *blogService) CreatePost(ctx context.Context, post *models.CreatePost) (*db.Post, error) {
+	newPost := db.CreatePostParams{
+		Title:    post.Title,
+		Content:  post.Content,
+		Category: post.Category,
+		Tags:     post.Tags,
+	}
 
+	resp, err := bs.dbQueries.CreatePost(ctx, newPost)
+	return &resp, err
 }
 
-func (bs *blogService) PutBlog() {
-
+func (bs *blogService) ReadAllPosts(ctx context.Context) (*[]db.Post, error) {
+	resp, err := bs.dbQueries.ListPosts(ctx)
+	return &resp, err
 }
 
-func (bs *blogService) GetBlog() {
-
+func (bs *blogService) ReadPostById(ctx context.Context) error {
+	return nil
 }
 
-func (bs *blogService) DeleteBlog() {
+func (bs *blogService) ReadPostByFilter(ctx context.Context) error {
+	return nil
+}
 
+func (bs *blogService) UpdatePost(ctx context.Context) error {
+	return nil
+}
+
+func (bs *blogService) DeleteBlog(ctx context.Context) error {
+	return nil
 }
